83a523f7bf6de7f01bd3ab0ed53d6b96d0f9d6cf
Daniel Gusmao <danielgp20@hotmail.com>
2022-04-10 12:22:58 -0300

    enumarate python

---
+++ enumarate.py
@@ -0,0 +1,11 @@
+#Enumarate
+
+lista = ["Deus", "Roque", "Fátima", "Daniel", "Thalita"]
+
+for i in range(len(lista)):
+	print(i,lista[i])
+
+#Com enumarate
+
+for i, nome in enumerate(lista):
+	print(i,nome)
